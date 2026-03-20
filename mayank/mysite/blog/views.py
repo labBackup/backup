75644c50d8f37398dc89d5b70de567d8cdb6d37d
backupaccount <tempc122mail@gmail.com>
2026-03-20 14:18:16 +0530

    Commit all current changes

--- mayank/mysite/blog/views.py
+++ mayank/mysite/blog/views.py
@@ -1,19 +1,26 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import ListView
+from .forms import EmailPostForm
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'blog/post/list.html'
 
-def post_list(request):
-    posts = Post.published.all()
-    paginator = Paginator(posts, 2)
-    page_number = request.GET.get('page', 1)
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-    except PageNotAnInteger:
-        posts=paginator.page(1)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+# def post_list(request):
+#     posts = Post.published.all()
+#     paginator = Paginator(posts, 2)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     except PageNotAnInteger:
+#         posts=paginator.page(1)
+#     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
@@ -25,3 +32,13 @@ def post_detail(request, year, month, day, post):
         publish__day=day
         )
     return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
