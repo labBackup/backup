58191f57d9c2a3c59938db863def6cbcca7712bd
backupaccount <tempc122mail@gmail.com>
2026-03-26 18:00:57 +0530

    26th March

--- mayank/mysite/blog/views.py
+++ mayank/mysite/blog/views.py
@@ -1,26 +1,28 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Comment, Post
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 2
     template_name = 'blog/post/list.html'
 
-# def post_list(request):
-#     posts = Post.published.all()
-#     paginator = Paginator(posts, 2)
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     except PageNotAnInteger:
-#         posts=paginator.page(1)
-#     return render(request, 'blog/post/list.html', {'posts': posts})
+def post_list(request):
+    posts = Post.published.all()
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts=paginator.page(1)
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
@@ -31,7 +33,9 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
         )
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post,'comments':comments, 'form':form})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
@@ -59,3 +63,14 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent':sent})
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form, 'comment':comment})
