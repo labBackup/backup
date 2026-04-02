5d7c211defcd3edac9ac061edf6c43b76840fa2d
backupaccount <tempc122mail@gmail.com>
2026-04-02 18:02:24 +0530

    feat: 2nd April

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'taggit',
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
+    'home'
 ]
 
 MIDDLEWARE = [
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog'))
+    path('blog/', include('blog.urls', namespace='blog')),
+    path('', include('home.urls', namespace='home')),
 ]
